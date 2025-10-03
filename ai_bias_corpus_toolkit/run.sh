f89996090f40b6c23a23b3b7b34a01f6badff010
flefichoux <fabien.lefichoux@innoval.com>
2025-10-03 16:39:50 +0200

    Delete aull mis

--- ai_bias_corpus_toolkit/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-SINCE=2015-05-01
-UNTIL=$(date +%F)
-OUTRAW=data/raw
-OUTCLEAN=data/clean/corpus.csv
-
-python harvest.py --since $SINCE --until $UNTIL --config config.yaml --out $OUTRAW
-python clean_merge.py --config config.yaml --raw_dir $OUTRAW --out $OUTCLEAN --extract-text
